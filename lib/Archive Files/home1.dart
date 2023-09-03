a0e8b58b8b4ee5319e79e9e2119389b7f2b1ce6d
CMDR-VanderWaal <mmkapasi53@yahoo.com>
2023-09-03 21:02:53 +0530

    smol changes

--- lib/Archive Files/home1.dart
+++ lib/Archive Files/home1.dart
@@ -1,63 +1,63 @@
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
 
-import '../ui/login.dart';
+// import '../ui/login.dart';
 
-class Home extends StatelessWidget {
-  Home({super.key});
+// class Home extends StatelessWidget {
+//   Home({super.key});
 
-  final Box _boxLogin = Hive.box("login");
+//   final Box _boxLogin = Hive.box("login");
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login App"),
-        elevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white),
-              ),
-              child: IconButton(
-                onPressed: () {
-                  _boxLogin.clear();
-                  _boxLogin.put("loginStatus", false);
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return const Login();
-                      },
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.logout_rounded),
-              ),
-            ),
-          )
-        ],
-      ),
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "Welcome 🎉",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              _boxLogin.get("userName"),
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Login App"),
+//         elevation: 0,
+//         actions: [
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: DecoratedBox(
+//               decoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 border: Border.all(color: Colors.white),
+//               ),
+//               child: IconButton(
+//                 onPressed: () {
+//                   _boxLogin.clear();
+//                   _boxLogin.put("loginStatus", false);
+//                   Navigator.pushReplacement(
+//                     context,
+//                     MaterialPageRoute(
+//                       builder: (context) {
+//                         return const Login();
+//                       },
+//                     ),
+//                   );
+//                 },
+//                 icon: const Icon(Icons.logout_rounded),
+//               ),
+//             ),
+//           )
+//         ],
+//       ),
+//       backgroundColor: Theme.of(context).colorScheme.primary,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: [
+//             Text(
+//               "Welcome 🎉",
+//               style: Theme.of(context).textTheme.bodyLarge,
+//             ),
+//             const SizedBox(height: 10),
+//             Text(
+//               _boxLogin.get("userName"),
+//               style: Theme.of(context).textTheme.headlineLarge,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
