159347080ec450772e227ec366bd04e06617b2cb
CMDR-VanderWaal <mmkapasi53@yahoo.com>
2024-10-21 00:50:02 +0530

    fixed Loading animation

--- lib/ui/dateSelect.dart
+++ lib/ui/dateSelect.dart
@@ -187,7 +187,9 @@ class _DateSelectorState extends State<DateSelector> {
       ),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else if (snapshot.hasData) {
